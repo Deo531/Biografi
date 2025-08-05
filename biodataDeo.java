ee4a026de967d856389b80e35db83fb424517829
Amadeo Abqary Dhiyaulhaq <deo531.id@gmail.com>
2025-08-05 11:28:37 +0700

    Update biodataDeo.java

--- biodataDeo.java
+++ biodataDeo.java
@@ -1,10 +1,11 @@
 public class biografiDeo {
     public static void main(String[] args) {
         System.out.println("Nama: Amadeo Abqary Dhiyaulhaq");
-        System.out.println("Asal Sekoalah: MTsN 1 B ");
+        System.out.println("Asal Sekoalah: MTsN 1 Blitar ");
         System.out.println("Alamat: Jalan Raya Gandekan Desa Gandekan Kec. Wonodadi Kab. Blitar");
         System.out.println("Jenis Kelamin: Laki-laki");
         System.out.println("Cita-cita: Ingin menjadi seorang programmer");
         System.out.println("Hobi: Bermain game dan mendesign");
     }
 }
+
