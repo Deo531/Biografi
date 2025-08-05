74fb8f4273ffcab17bb302d512d7701e9d9984a7
Amadeo Abqary Dhiyaulhaq <deo531.id@gmail.com>
2025-08-05 08:44:38 +0700

    Add files via upload

---
+++ biografiDeo.java
@@ -0,0 +1,10 @@
+public class biografiDeo {
+    public static void main(String[] args) {
+        System.out.println("Nama: Amadeo Abqary Dhiyaulhaq");
+        System.out.println("Asal Sekoalah: MTsN 1 B ");
+        System.out.println("Alamat: Jalan Raya Gandekan Desa Gandekan Kec. Wonodadi Kab. Blitar");
+        System.out.println("Jenis Kelamin: Laki-laki");
+        System.out.println("Cita-cita: Ingin menjadi seorang programmer");
+        System.out.println("Hobi: Bermain game dan mendesign");
+    }
+}
